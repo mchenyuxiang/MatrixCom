223dba7731a639137d290df08ea6e1a6f1fe57df
mchenyuxiang <csyxchen@qq.com>
2017-08-09 10:14:04 +0800

    evaluate

---
+++ matrix/evaluate/Evaluate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/9 10:13
+# @Author  : mchenyuxiang
+# @Email   : mchenyuxiang@foxmail.com
+# @Site    : 
+# @File    : Evaluate.py
+# @Software: PyCharm
+
+import numpy as np
+
+def test_error(computer_matrix,test_matrix):
+    idx = np.where(test_matrix > 0)
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ matrix/evaluate/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/9 9:57
+# @Author  : mchenyuxiang
+# @Email   : mchenyuxiang@foxmail.com
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
