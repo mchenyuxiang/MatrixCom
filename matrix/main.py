ad24a9b392856c7da7f637fe4c19ed9a449fa287
mchenyuxiang <csyxchen@qq.com>
2017-08-11 03:23:18 +0800

    重复计算添加权重

--- matrix/main.py
+++ matrix/main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     ## 将兴趣归一矩阵利用LSH哈希函数将矩阵分块
     k_number = 10
     w = 1
-    combin_number = 2
+    combin_number = 4
     b = np.random.uniform(0, w)
     test_lsh_index = LSH.lsh_bucket(user_style_matrix, k_number, w, b)
     lsh_split = LSH.lsh_bucket_split(test_lsh_index)
@@ -45,7 +45,7 @@ if __name__ == "__main__":
         R = re_test_matrix[loc[i, 1]:(loc[i, 2] + 1), :]
         N = len(R)
         M = len(R[0])
-        K = 15
+        K = 10
         P = np.random.rand(N, K)
         Q = np.random.rand(M, K)
         nP, nQ = SGD.SGD(R, P, Q, K)
@@ -53,15 +53,34 @@ if __name__ == "__main__":
         if i == 0:
             final_matrix_temp = result
         else:
+            final_matrix_temp = np.vstack((final_matrix_temp, result[(lsh_split.astype(np.int)[i + combin_number-1, 1]-lsh_split.astype(np.int)[i, 1]):(lsh_split.astype(np.int)[i + combin_number-1, 2]+1-lsh_split.astype(np.int)[i, 1]),:]))
             last_center = (lsh_split[i - 1, 0] + w * combin_number) / 2  # 上一轮中心点
             this_center = (lsh_split[i, 0] + w * combin_number) / 2  # 本轮中心点
-            distance_split_matrix = test_lsh_index[lsh_split.astype(np.int)[i,1]:(lsh_split.astype(np.int)[i+combin_number-1,2]),:] # 提取重复计算行数
-            last_distance = distance_split_matrix[:,1]-last_center
-            this_distance = distance_split_matrix[:,1]-this_center
-            last_result = final_matrix_temp[lsh_split.astype(np.int)[i,1]:(lsh_split.astype(np.int)[i+combin_number-1,2]),:] # 上一轮计算出的结果
-            this_result = result[0:(lsh_split.astype(np.int)[i+combin_number-1,2]-lsh_split.astype(np.int)[i,1]+1),:] # 本轮计算结果
-            new_result = last_result*(last_distance/(last_distance+this_distance))+this_result*(this_distance/(last_distance+this_distance))
-            final_matrix_temp = np.vstack((final_matrix_temp, new_result))
+            distance_split_matrix = test_lsh_index[
+                                    lsh_split.astype(np.int)[i, 1]:(lsh_split.astype(np.int)[i + combin_number - 1, 1]),
+                                    :]  # 提取重复计算行数
+            last_distance = distance_split_matrix[:, 1] - last_center
+            this_distance = distance_split_matrix[:, 1] - this_center
+            last_result = final_matrix_temp[
+                          lsh_split.astype(np.int)[i, 1]:(lsh_split.astype(np.int)[i + combin_number - 1, 1]),
+                          :]  # 上一轮计算出的结果
+            this_result = result[
+                          0:(lsh_split.astype(np.int)[i + combin_number - 1, 1] - lsh_split.astype(np.int)[i, 1]),
+                          :]  # 本轮计算结果
+            last_weight_temp = (last_distance / (last_distance + this_distance))
+            this_weight_temp = (this_distance / (last_distance + this_distance))
+            last_weight = np.ones(last_result.shape)
+            this_weight = np.ones(this_result.shape)
+            for j in range(len(last_weight_temp)):
+                last_weight[j] = last_weight_temp[j] * last_weight[j]
+                this_weight[j] = this_weight_temp[j] * this_weight[j]
+            new_result = last_result * last_weight + this_result * this_weight
+            result[
+            0:(lsh_split.astype(np.int)[i + combin_number - 1, 1] - lsh_split.astype(np.int)[i, 1]),
+            :] = new_result
+            final_matrix_temp = np.vstack((final_matrix_temp[
+                                           0:lsh_split.astype(np.int)[i - 1, 2] + 1,
+                                           :], result))
         print("times:%d" % i)
     # print(lsh_split)
     # print(re_test_matrix)
@@ -79,6 +98,6 @@ if __name__ == "__main__":
     # direct_sgd_mc = np.dot(nP, nQ.T)
     ## 评价
     lsh_test_error = EVA.test_error(final_matrix, user_test_rank_matrix)
-    # direct_test_error = EVA.test_error(direct_sgd_mc,user_test_rank_matrix)
+    # direct_test_error = EVA.test_error(direct_sgd_mc, user_test_rank_matrix)
     print("lsh:%f\n" % lsh_test_error)
-    # print("sgd:%f\n"%direct_test_error)
+    # print("sgd:%f\n" % direct_test_error)
