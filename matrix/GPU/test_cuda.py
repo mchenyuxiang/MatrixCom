1fb2d151edd6ddc7dd95bee42122e7c293a72847
mchenyuxiang <csyxchen@qq.com>
2017-08-23 07:41:04 +0800

    以函数的方式调用gpu

--- matrix/GPU/test_cuda.py
+++ matrix/GPU/test_cuda.py
@@ -1,5 +1,5 @@
 import pycuda.autoinit
-import pycuda.driver as drv
+import pycuda.driver as cuda
 import numpy
 
 from pycuda.compiler import SourceModule
@@ -12,15 +12,19 @@ __global__  void multiply_them(float *dest, float *a, float *b)
 } 
 """)
 
-multiply_them = mod.get_function("multiply_them")
+def cuda_add():
+    multiply_them = mod.get_function("multiply_them")
 
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
+    a = numpy.random.randn(400).astype(numpy.float32)
+    b = numpy.random.randn(400).astype(numpy.float32)
 
-dest = numpy.zeros_like(a)
-multiply_them(
-    drv.Out(dest), drv.In(a), drv.In(b),
-    block=(400, 1, 1), grid=(1, 1))
+    dest = numpy.zeros_like(a)
+    multiply_them(
+        cuda.Out(dest), cuda.In(a), cuda.In(b),
+        block=(400, 1, 1), grid=(1, 1))
+    return dest
 
-print(dest)
+if __name__ == "__main__":
+    dest = cuda_add()
+    print(dest)
 # print(dest-a*b)
