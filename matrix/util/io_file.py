edabce07c1a9bca6ad0d4736fa97aa71a38a84a5
mchenyuxiang <csyxchen@qq.com>
2017-08-22 20:37:18 +0800

    单独处理文件的读写

--- matrix/util/io_file.py
+++ matrix/util/io_file.py
@@ -12,6 +12,12 @@ from scipy import io
 
 def save_mat(file_url,save_url,extra_name):
     mat = io.loadmat(file_url)
-    mat_t = np.transpose(mat[extra_name])
+    mat_t = mat[extra_name]
     save_name = save_url+"/"+extra_name+".npy"
     np.save(save_name,mat_t)
+
+if __name__ == "__main__":
+    file_url = '../dataset/pings_4hr/pings_4h30min.mat'
+    save_url = '../dataset/pings_4hr'
+    extra_name = 'originMatrix4h30min'
+    save_mat(file_url,save_url,extra_name)
